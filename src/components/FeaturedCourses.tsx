5a792176ef8b91296830857c62bf1eee98d12ac2
AnupriyaBiswas <anupriyabiswas2206@gmail.com>
2024-11-01 12:58:07 +0530

    features courses added

---
+++ src/components/FeaturedCourses.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link"
+import courseData from "../data/music_courses.json"
+
+function FeaturedCourses() {
+  return (
+    <div className="py-12 bg-gray-900">
+        <div>
+          <div className="text-center">
+            <h2 className="text-base text-teal-600 font-semibold tracking-wide uppercase">
+              FEATURED COURSES
+            </h2>
+
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-white sm:text-4xl">
+              Learn With The Best
+            </p>
+          </div>
+        </div>
+
+        <div>2</div>
+
+        <div className="mt-20 text-center">
+          <Link href={"/courses"}>
+            View All Courses
+          </Link>
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedCourses
