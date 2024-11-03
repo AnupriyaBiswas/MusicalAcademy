cb853d0c220e97acc5e614936019ddc0cfae3596
AnupriyaBiswas <anupriyabiswas2206@gmail.com>
2024-11-03 15:16:23 +0530

    Testimonials and Moving Cards

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -28,12 +28,18 @@ const config: Config = {
     extend: {
       animation:{
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite"
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":"conic-gradient(from 180deg at 50% 50%, var (--tw-gradient-stops))",
       },
       keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
         spotlight: {
           "0%": {
             opacity: '0',
